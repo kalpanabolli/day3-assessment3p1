be297a8c1ef10082232b2b15b898840e3b5b85b0
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-07-14 18:18:29 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment3p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine("Parsed Number using intTryParse is :\t" + result);
+                }
+                else
+                {
+                    Console.WriteLine("Faile to Parse");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!" + ex.Message);
+            }
+            try
+            {
+                string convertString;
+                Console.WriteLine("Enter the String");
+                convertString = Console.ReadLine();
+                double Value = Convert.ToDouble(convertString);
+                Console.WriteLine("Convert successful. Converted double value: " + Value);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Convert failed. Invalid input format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Convert failed. Input value is too large or too small.");
+            }
+
+            try
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+
+                decimal Value = Convert.ToDecimal(input);
+                Console.WriteLine("Parse successful. Converted decimal value: " + Value);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Parse failed. Invalid input format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Parse failed. Input value is too large or too small.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
